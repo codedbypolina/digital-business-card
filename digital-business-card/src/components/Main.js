034105c2d2e0ec13fca41e1bf738ff32ed297bcf
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-02-13 17:39:57 -0500

    Styling main content

--- digital-business-card/src/components/Main.js
+++ digital-business-card/src/components/Main.js
@@ -1,12 +1,19 @@
 export default function Main() {
     return (
         <div className="main">
-            <h1>Polina Shliakhina</h1>
-            <h2>Frontend Developer</h2>
-            <h3>codedbypolina.com</h3>
+            <div className="name">
+                <h1>Polina Shliakhina</h1>
+                <h2>Frontend Developer</h2>
+                <h3>codedbypolina.com</h3>
+            </div>
 
-            <button>Email</button>
-            <button>LinkedIn</button>
+            <div className="buttons">
+                <button>
+                    Email
+                </button>
+                <button className="linkedin">LinkedIn</button>
+            </div>
+
 
             <h4>About</h4>
             <p></p>
